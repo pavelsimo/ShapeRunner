214b7d320952f2a6ce022676c39c8ff145124748
Pavel Simo <pavel.simo@gmail.com>
2025-03-23 00:17:21 +0100

    🚧 clean up

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-import { Game } from './game.js';
-
-// Initialize the game when the window loads
-window.addEventListener('load', () => {
-    const game = new Game();
-    game.initialize();
-    game.startGameLoop();
-}); 
